024efdadc446c35ce3200605b55c047a018a2bf4
unknown <wassouf.ryan@outlook.fr>
2024-11-18 12:07:10 +0100

    update database

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,14 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASS, {
         host: process.env.DB_HOST,
-        dialect: 'postgres'
+        dialect: 'postgres',
+        dialectModule: require('pg'),
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
     }
 )
 
