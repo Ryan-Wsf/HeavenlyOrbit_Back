972d964c1ffcc50e93232ca02d76eef25b7e69d1
unknown <wassouf.ryan@outlook.fr>
2024-11-18 11:33:46 +0100

    update back

--- src/config/database.js
+++ src/config/database.js
@@ -6,8 +6,17 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASS, {
         host: process.env.DB_HOST,
-        dialect: 'mysql'
+        dialect: 'postgres'
     }
 )
+
+(async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('Connexion à PostgreSQL réussie !');
+    } catch (error) {
+      console.error('Impossible de se connecter à PostgreSQL :', error);
+    }
+})();
 
 module.exports = sequelize;
